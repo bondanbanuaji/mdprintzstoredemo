2252605cf7e9695b978b6d219f0fb226402df9d4
Boba? <bondanbanuaji@gmail.com>
2025-05-12 13:23:06 +0700

    add banner section

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import deal from '../assets/deal-bg.jpg';
+
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 const Banner = () => {
+
+  useEffect(() => {
+          AOS.init({
+              offset: 100,
+              duration: 500,
+              easing: 'ease-in-out',
+          })
+  
+          AOS.refresh();
+  
+      }, [])
+
   return (
-    <div>Banner</div>
+    <div className='w-full lg:px-20 px-5 py-[80px]'>
+      <div data-aos="zoom-in" data-aos-delay="100" className='w-full h-[300px] rounded-lg bg-cover bg-center flex flex-col justify-center items-center gap-3'
+      style={{ backgroundImage: `url(${deal})` }}>
+        <h1 className='text-xl font-semibold text-themeyellow'>EverdyDay Shopping</h1>
+        <h1 className='font-bold text-white text-[42px] leading-[50px] text-center'>Deal of the day</h1>
+      </div>
+    </div>
   )
 }
 
