5c9edc264d77c4f7037eea29f3d0dfdb4a9db686
Boba? <bondanbanuaji@gmail.com>
2025-05-14 19:38:10 +0700

    add insta section

--- src/sections/Insta.jsx
+++ src/sections/Insta.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import insta1 from '../assets/insta-1.jpg';
+import insta2 from '../assets/insta-2.jpg';
+import insta3 from '../assets/insta-3.jpg';
+import insta4 from '../assets/insta-4.jpg';
+import insta5 from '../assets/insta-5.jpg';
+import insta6 from '../assets/insta-6.jpg';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const Insta = () => {
+
+    useEffect(() => {
+      AOS.init({
+        offset: 100,
+        duration: 500,
+        easing: 'ease-in-out',
+      })
+  
+      AOS.refresh();
+  
+    }, [])
+  
+
   return (
-    <div>Insta</div>
+    <div className='w-full lg:px-20 px-5 py-[80px] bg-white flex flex-col justify-center items-center gap-4'>
+      <h1 data-aos="zoom-in" data-aos-delays="100" 
+      className='text-xl font-semibold capitalize text-themepurple' >Our Instagram Shop
+      </h1>
+      <h1 data-aos="zoom-in" data-aos-delay="200"
+        className='font-semibold text-black text-[42px] leading-[50px] text-center capitalize'>
+        Follow On Facebook
+      </h1>
+      <div data-aos="zoom-in" data-aos-delay="300"
+        className='grid items-center justify-center w-full grid-cols-1 gap-6 mt-8 frid lg:grid-cols-6'>
+        <img src={insta1} alt="" className='rounded-lg' />
+        <img src={insta2} alt="" className='rounded-lg' />
+        <img src={insta3} alt="" className='rounded-lg' />
+        <img src={insta4} alt="" className='rounded-lg' />
+        <img src={insta5} alt="" className='rounded-lg' />
+        <img src={insta6} alt="" className='rounded-lg' />
+      </div>
+      <button data-aos="zoom-in" data-aos-delay="400"
+      className='px-8 py-3 mt-12 font-semibold text-white uppercase rounded-lg bg-themepurple hover:bg-themeyellow hover:text-black'> #Mdprintz </button>
+    </div>
   )
 }
 
